4995610c47793c2abfa479b9640af9d90ffe14fd
gurkirat61 <981834@pdsb.net>
2024-09-19 13:01:28 -0400

    Create lesson3.java

---
+++ lesson3.java
@@ -0,0 +1,18 @@
+class Testing {
+  public static void main(String [] args) {
+    String name = "Gurkirat";
+    System.out.println("Hello " + name);
+    int x; // declaration
+    x = 12; // assignment
+    System.out.println(x); // print to console
+    System.out.println("There are " + x + " inches in a foot.");
+    System.out.println("What mammal has the most teeth?");
+    System.out.print("The Giant Armadillo has ");
+    System.out.print(74);
+    System.out.println(" teeth.");
+    System.out.println();
+    System.out.println("Welcome to Sanjay's Koala Party!");
+    System.out.print("It's possible Sanjay can distribute " + 12);
+    System.out.println(" cookies for each bowl.");
+    System.out.println("There are " + 28 + " people in this room.");
+    System.out.println("Your total money spent is: " + (2.75+9.32+11.91));
