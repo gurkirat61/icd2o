a491d33a9b07e13b2aa1859f101a82390f4c0cea
gurkirat61 <981834@pdsb.net>
2024-09-24 14:20:20 -0400

    Create assignment1.java

---
+++ assignment1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1;
+import java.util.Scanner;
+/**
+ *
+ * @author 981834
+ */
+public class Assignment1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input the rectangles length");
+        System.out.print("Input the rectangles wigth");
+        length = sc.nextInt();
+    }
+    
+}
