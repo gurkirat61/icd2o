9f552cc89417ffc0cc97d7d7345ea147c921419a
gurkirat61 <981834@pdsb.net>
2024-09-30 13:50:01 -0400

    Create assignment4.java

---
+++ assignment4.java
@@ -0,0 +1,42 @@
+package javaapplication5;
+import java.lang.Math;
+import java.util.Scanner;
+/**
+ *
+ * @author 957353
+ */
+public class JavaApplication5{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int length;
+        int width;
+        System.out.print("input length of square: ");
+        length = sc.nextInt();
+        System.out.print("input width of square: ");
+        width = sc.nextInt();
+        System.out.println(length*width);
+        int radius;
+        System.out.print("input radius of circle: ");
+        radius = sc.nextInt();
+        System.out.println(Math.PI*(radius*radius));
+        int cradius;
+        int height;
+        System.out.print("input radius of cylinder: ");
+        cradius = sc.nextInt();
+        System.out.print("input height of cylinder: ");
+        height = sc.nextInt();
+        System.out.println(Math.PI*(cradius*cradius)*height);
+        int L;
+        double G;
+        G = 9.8;
+        System.out.print("input meters: ");
+        L = sc.nextInt();
+        System.out.print((Math.PI*Math.PI)Math.sqrt(L/G));
+    }
+   
+}
